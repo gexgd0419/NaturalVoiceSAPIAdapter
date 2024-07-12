d5cf6a31fce079abcd01b8d9dad8bb8033862407
gexgd0419 <55008943+gexgd0419@users.noreply.github.com>
2024-07-12 21:43:58 +0800

    Update precompile header file to speed up compiling

--- NaturalVoiceSAPIAdapter/framework.h
+++ NaturalVoiceSAPIAdapter/framework.h
@@ -24,10 +24,33 @@
 
 #define RETONFAIL(hresult) if (HRESULT __hr = hresult; FAILED(__hr)) return __hr
 
+// C++ standard library headers
 #include <memory>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <thread>
+#include <future>
+#include <functional>
+#include <system_error>
+#include <format>
 
 using ATL::CComPtr; // put this here to avoid 'CComPtr undeclared' in spddkhlp.h
 #pragma warning(suppress : 4996) // 'GetVersionExW' deprecated
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <spddkhlp.h>
+#include <ShObjIdl.h>
+
+#include <winrt/windows.management.deployment.h>
+#include <winrt/windows.applicationmodel.h>
+#include <winrt/windows.foundation.collections.h>
+
+// Third-party libraries
+#include <speechapi_cxx.h>
+#define ASIO_STANDALONE 1
+#include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_client.hpp>
+#include <asio.hpp>
+#include <nlohmann/json.hpp>
